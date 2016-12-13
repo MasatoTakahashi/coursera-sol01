7840e653309b3fdd293c130724f2b8160383e272
Takahashi, Masato a <masato.a.takahashi@accenture.com>
2016-12-13 22:43:00 +0900

    Implement bonus fuunctions

--- app.js
+++ app.js
@@ -2,27 +2,42 @@
   'use strict';
   
   angular.module("LunchCheck", [])
-  .controller("LunchCheckckController", ['$scope', '$log', function($scope, $log) {
+  .controller("LunchCheckckController", LunchCheckckController); 
+  
+  // inject the dependency
+  LunchCheckckController.$inject = ['$scope', '$log'];
+  
+  function LunchCheckckController($scope, $log) {
     $scope.message = '';
     $scope.inputItems = '';
-    
+              
     $scope.CheckNItem = function(){
-      $log.log($scope.inputItems);
       
       if($scope.inputItems === ''){
         $scope.message = 'Please enter data first';
+        $scope.messageColour = {'color':'red'};
+        $scope.checkButtonColour = {'border':'solid 2px red'};
       }else{
-        var nItems = $scope.inputItems.split(',').length;
+        // split by comma
+        var splitItems = $scope.inputItems.split(',');
+        // remove whitespace or empty element
+        splitItems = splitItems.filter(function(str){
+          return /\S/.test(str);
+        }).filter(Boolean);
+        
+        var nItems = splitItems.length;
         var threshold_nItems = 3;
+        
         if(nItems > threshold_nItems){
           $scope.message = 'Too much!';
         }else if(nItems <= threshold_nItems){
           $scope.message = 'Enjoy!';
         }
+        $scope.messageColour = {'color':'green'};
+        $scope.checkButtonColour = {'border':'solid 2px green'};
+        
       }
-      
-    };
-    
-  }]);
+    }    
+  }
   
 })();
